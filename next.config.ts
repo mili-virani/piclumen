c6fc354b4cf73dc2a3dcb269e68b812b507a2901
Mili <milivirani1911@gmail.com>
2025-03-25 12:12:38 +0530

    Fixed ESLint errors and warnings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "", // Requests to /api/get_photos
+        destination: "http://68.183.93.60:7979/", // Proxies to backend
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
